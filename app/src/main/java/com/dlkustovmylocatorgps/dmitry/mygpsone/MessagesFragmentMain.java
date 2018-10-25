6da585102217f2628b6540726bfce2e70856b11c
Dmitry Kustov <dlkustov@gmail.com>
2018-10-25 23:18:27 +0300

    Second changes!

--- app/src/main/java/com/dlkustovmylocatorgps/dmitry/mygpsone/MessagesFragmentMain.java
+++ app/src/main/java/com/dlkustovmylocatorgps/dmitry/mygpsone/MessagesFragmentMain.java
@@ -2,6 +2,7 @@ package com.dlkustovmylocatorgps.dmitry.mygpsone;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,25 +13,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 public class MessagesFragmentMain extends Fragment {
     private Button btnClick;
+    private Button buttonUploadFile;
     private EditText editTextOutMsg;
     private TextView editTextIncomingMsg;
     private DatabaseReference mDatabase;
     private DatabaseReference mDatabaseIncoming;
+    //mDatabaseReference
+    private DatabaseReference mDatabaseReference;
+
+    //these are the views
+    EditText editTextFilename;
+    ProgressBar progressBar;
+    TextView textViewStatus;
+
+    // Для работы с облаком FirebaseStorage
+    private FirebaseStorage m_myStorage;
+    private StorageReference m_myRootRef;
+    //this is the pic pdf code used in file chooser
+    final static int PICK_PDF_CODE = 2342;
 
     StorageReference mStorageReference;
 
@@ -40,12 +64,46 @@ public class MessagesFragmentMain extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
+        m_myStorage = FirebaseStorage.getInstance();
+        m_myRootRef = m_myStorage.getReference();
+        //getting firebase objects
+        //mStorageReference = FirebaseStorage.getInstance().getReference();
+       //mDatabaseReference = FirebaseDatabase.getInstance().getReference(com.dlkustovmylocatorgps.dmitry.mygpsone.Constants.DATABASE_PATH_UPLOADS);
+
+       // mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
         View retView = inflater.inflate(R.layout.fragment_massages_main, container, false);
 
         btnClick = (Button)retView.findViewById(R.id.buttonSendMsg);
+        buttonUploadFile = (Button)retView.findViewById(R.id.buttonUploadFile);
         editTextOutMsg = (EditText)retView.findViewById(R.id.inputMsgText);
         editTextIncomingMsg = (TextView)retView.findViewById(R.id.textViewIncoming);
         editTextIncomingMsg.setMovementMethod(new ScrollingMovementMethod());
+
+
+        //getting the views
+        textViewStatus = (TextView)retView.findViewById(R.id.textViewStatus);
+        editTextFilename = (EditText)retView.findViewById(R.id.editTextFileName);
+        progressBar = (ProgressBar)retView.findViewById(R.id.progressbar);
+
+        //attaching listeners to views
+       //retView.findViewById(R.id.buttonUploadFile).setOnClickListener();
+        buttonUploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Test.pdf");
+                MyUploadFile("Test.pdf");
+                /*switch (view.getId()) {
+                    case R.id.buttonUploadFile:
+                        //getPDF();
+                        break;
+                    //case R.id.textViewUploads:
+                       // startActivity(new Intent(this, ViewUploadsActivity.class));
+                    //    break;
+                }*/
+            }
+        });
+
 
         btnClick.setOnClickListener(new View.OnClickListener()
         {
@@ -106,10 +164,48 @@ public class MessagesFragmentMain extends Fragment {
 
         return retView;
     }
-   /* private void uploadFile(Uri data) {
+    private void MyUploadFile(String stMyPath)
+    {
+       /* m_myStorage = FirebaseStorage.getInstance();
+        StorageReference rootRef = m_myStorage.getReference();*/
+
+        File sdPath = Environment.getExternalStorageDirectory();
+        sdPath = new File(sdPath.getAbsolutePath() + "/Downloads/"); // dir
+       // File file = new File(sdPath, "test.txt"); //имя файла
+        File file = new File(sdPath, stMyPath); //имя файла
+
+        try
+        {
+            InputStream stream = new FileInputStream(file);
+            StorageReference mountainsRef = m_myRootRef.child("test");
+            UploadTask uploadTask = mountainsRef.putStream(stream);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Uri downloadUrl = taskSnapshot.getUploadSessionUri();
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            System.out.print(ex.getMessage());
+        }
+
+
+
+    }
+    //this method is uploading the file
+    //the code is same as the previous tutorial
+    //so we are not explaining it
+    private void uploadFile(Uri data) {
         //progressBar.setVisibility(View.VISIBLE);
-        // STORAGE_PATH_UPLOADS
-        StorageReference sRef = mStorageReference.child(Constants.STORAGE_PATH_UPLOADS + System.currentTimeMillis() + ".pdf");
+        StorageReference sRef = mStorageReference.child(com.dlkustovmylocatorgps.dmitry.mygpsone.Constants.STORAGE_PATH_UPLOADS + System.currentTimeMillis() + ".pdf");
         sRef.putFile(data)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @SuppressWarnings("VisibleForTests")
@@ -118,14 +214,14 @@ public class MessagesFragmentMain extends Fragment {
                         progressBar.setVisibility(View.GONE);
                         textViewStatus.setText("File Uploaded Successfully");
 
-                        Upload upload = new Upload(editTextFilename.getText().toString(), taskSnapshot.getDownloadUrl().toString());
+                        Upload upload = new Upload(editTextFilename.getText().toString(), taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
                         mDatabaseReference.child(mDatabaseReference.push().getKey()).setValue(upload);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception exception) {
-                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 })
                 .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
@@ -137,6 +233,6 @@ public class MessagesFragmentMain extends Fragment {
                     }
                 });
 
-    }*/
+    }
 
 }
